b6b63babb7c0c67315691bf707da126630976279
JJBlue <lieb.julian@gmx.de>
2020-01-20 11:42:52 +0100

    update

--- src/crossplatformapi/jni/display/NativeDisplay.java
+++ src/crossplatformapi/jni/display/NativeDisplay.java
@@ -1,11 +1,25 @@
 package crossplatformapi.jni.display;
 
 public class NativeDisplay {
-	public native static long getMainDisplay();
+	public native static long getPrimaryDisplay();
+	public native static long getDisplay(long window);
+	public native static long getDisplay(long x, long y);
 	public native static long[] getDisplays();
 	
-	public native static long getX();
-	public native static long getY();
-	public native static long getWidth();
-	public native static long getHeight();
+	public native static boolean isPrimary(long monitor);
+	
+	public native static long getX(long monitor);
+	public native static long getY(long monitor);
+	public native static long getWidth(long monitor);
+	public native static long getHeight(long monitor);
+	
+	/**
+	 * @return the workarea of a monitor.
+	 * For example Windows is the workarea without the taskbar.
+	 * In some cases Workarea is the same as the normal area
+	 */
+	public native static long getWorkX(long monitor);
+	public native static long getWorkY(long monitor);
+	public native static long getWorkWidth(long monitor);
+	public native static long getWorkHeight(long monitor);
 }
