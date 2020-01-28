c057302bf5f689f58758e874d19e9b14b2602989
JJBlue <lieb.julian@gmx.de>
2020-01-28 12:41:11 +0100

    Add get Network SSIDs

--- src/crossplatformapi/jni/network/Network.java
+++ src/crossplatformapi/jni/network/Network.java
@@ -5,5 +5,7 @@ public class Network {
 	 * @return if connected to any network
 	 */
 	public native static boolean isConnected();
-//	public native static String getCurrentNetworkName();
+	
+	public native static String[] getConnectedSSIDs();
+	public native static String[] getAllSSIDs();
 }
